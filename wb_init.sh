157cb6f7b482d857dc0ec27270281e65719f2a49
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-02-11 07:58:35 +0100

    prepare init-script for new docker-compose version

--- wb_init.sh
+++ wb_init.sh
@@ -6,4 +6,8 @@
 
 set -e
 
+# *** bootUp for version < 1.6.n o docker-compose
 docker-compose --x-networking --x-network-driver=bridge up -d
+
+# *** bootUp for version >= 1.6.n of docker-compose
+# docker-compose --file docker-composer-v2.yml up -d
