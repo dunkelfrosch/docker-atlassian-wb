7fbd4945e25753077902c1002d2bdb67f79f281c
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-03-08 09:20:42 +0100

    fix minor workbench init script issues

--- wb_init.sh
+++ wb_init.sh
@@ -7,7 +7,7 @@
 set -e
 
 # *** bootUp for version < 1.6.n o docker-compose
-docker-compose --x-networking --x-network-driver=bridge up -d
+# docker-compose --x-networking --x-network-driver=bridge up -d
 
 # *** bootUp for version >= 1.6.n of docker-compose
-# docker-compose --file docker-composer-v2.yml up -d
+docker-compose --file docker-compose.yml up -d
